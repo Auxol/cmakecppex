e4fe6cedc4d8e699801a229639c519f3323a79bf
Auxol <auxol@proton.me>
2023-11-11 01:24:49 -0800

    init

---
+++ build/CMakeFiles/BBC.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "BBC.exe"
+  "BBC.exe.manifest"
+  "BBC.pdb"
+  "CMakeFiles/BBC.dir/src/blackBalls/ball.cpp.obj"
+  "CMakeFiles/BBC.dir/src/blackBalls/ball.cpp.obj.d"
+  "CMakeFiles/BBC.dir/src/main.cpp.obj"
+  "CMakeFiles/BBC.dir/src/main.cpp.obj.d"
+  "CMakeFiles/BBC.dir/src/nerdShit/physics.cpp.obj"
+  "CMakeFiles/BBC.dir/src/nerdShit/physics.cpp.obj.d"
+  "libBBC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BBC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+using namespace std;
+
+int main(){
+
+    ball();
+
+    cout << "test" << endl;
+
+    return 0;
+}
